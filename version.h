8a49bb0a57ef9d7c6efc5d6632f429a7d4fd3972
Open Panzer <admin@openpanzer.org>
2018-07-07 19:32:41 -0500

    First version release 0.91.00

---
+++ version.h
@@ -0,0 +1,26 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+// LAST UPDATE: 7/7/2018
+
+#define VER_FILEVERSION              0,91,00,00
+#define VER_FILEVERSION_STR         "0.91.00.00\0"
+
+// THIS IS THE VERSION THAT WILL BE CHECKED AGAINST the appcast.xml version on the downloads page.
+// YOU MUST ALSO RUN QMAKE for this to update.
+#define VER_PRODUCTVERSION           0,91,00,00
+#define VER_PRODUCTVERSION_STR      "0.91.00\0"
+
+#define VER_COPYRIGHT_YEAR_STR      "2018"      // Change it below as well v
+#define VER_COMPANYNAME_STR         "OpenPanzer.org"
+#define VER_FILEDESCRIPTION_STR     "OP Sound INI Creator"
+#define VER_INTERNALNAME_STR        "OP Sound INI Creator"
+#define VER_LEGALCOPYRIGHT_STR      "Copyright (C) 2018 OpenPanzer.org"
+#define VER_LEGALTRADEMARKS1_STR    "All Rights Reserved"
+#define VER_LEGALTRADEMARKS2_STR    VER_LEGALTRADEMARKS1_STR
+#define VER_ORIGINALFILENAME_STR    "OPSound.exe"
+#define VER_PRODUCTNAME_STR         "OP Sound INI Creator"
+
+#define VER_COMPANYDOMAIN_STR       "openpanzer.org"
+
+#endif // VERSION_H
